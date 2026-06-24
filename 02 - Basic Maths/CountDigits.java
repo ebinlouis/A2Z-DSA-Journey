22a6a5c18dac0f2a62c5f8cba1293564ddbbf756
Ebin Louis <ebinlouis1559@gmail.com>
2026-06-24 18:27:37 +0530

    completed the countDigits in Basic Maths

---
+++ 02 - Basic Maths/CountDigits.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class CountDigits{
+    public static void main(String args[]){
+        System.out.println("Enter the number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        while ( n > 0){
+            n = n / 10;
+            count++;
+        }
+        System.out.println("Total digits: " + count);
+    }
+}
