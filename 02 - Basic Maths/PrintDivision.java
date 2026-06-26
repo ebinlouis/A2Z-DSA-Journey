c7c9dde206bcd2c0a151ae2a70bacb3abadd3bae
Ebin Louis <ebinlouis1559@gmail.com>
2026-06-26 22:25:07 +0530

    updated the PrintDivison.java completed

--- 02 - Basic Maths/PrintDivision.java
+++ 02 - Basic Maths/PrintDivision.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 class PrintDivision{
@@ -5,8 +7,18 @@ class PrintDivision{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
         int n = sc.nextInt();
-        for (int i = 1; i <= n; i++){
-            if (n % i == 0)System.out.print(i + "\t");
+        ArrayList<Integer> ls = new ArrayList<>();
+        for (int i = 1; i <= Math.sqrt(n); i++){
+            if (n % i == 0){
+                ls.add(i);
+                if (n / i == i)
+                    continue;
+                ls.add(n / i);
+            }
+        }
+        Collections.sort(ls);
+        for (int item: ls){
+            System.out.print(item + " ");
         }
         sc.close();
     }
